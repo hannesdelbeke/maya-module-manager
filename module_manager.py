6ce2b47042b0019d379ff0813abaec9e9fa0c9e3
hannes <hannesdelbeke@gmail.com>
2024-10-01 23:54:14 +0100

    convert to single maya plugin

--- module_manager.py
+++ module_manager.py
@@ -3,14 +3,226 @@ import webbrowser
 from functools import partial
 
 try:
+    import shiboken2 as shiboken
     from PySide2 import QtWidgets, QtGui, QtCore
 except:
+    import shiboken6 as shiboken
     from PySide6 import QtWidgets, QtGui, QtCore
     
+from maya import cmds
+from maya import OpenMayaUI
 
-from module_manager import utils
-from module_manager.constants import MAYA_ARGUMENTS
 
+# ----------------------CONSTANTS---------------------------------
+MODULE_ARGUMENTS = [
+    "MAYAVERSION",
+    "PLATFORM",
+    "LOCALE",
+]
+MAYA_ARGUMENTS = {
+    "MAYAVERSION": cmds.about(version=True),
+    "PLATFORM": cmds.about(operatingSystem=True),
+    "LOCALE": cmds.about(uiLanguage=True),
+}
+
+
+# ----------------------UTILS---------------------------------
+
+def get_main_window():
+    """
+    :return: Maya main window
+    :rtype: QtWidgets.QMainWindow/None
+    :raise RuntimeError: When the main window cannot be obtained.
+    """
+    ptr = OpenMayaUI.MQtUtil.mainWindow()
+    ptr = int(ptr)
+    if ptr:
+        return shiboken.wrapInstance(ptr, QtWidgets.QMainWindow)
+
+    raise RuntimeError("Failed to obtain a handle on the Maya main window.")
+
+
+def get_icon_path(file_name):
+    """
+    Get an icon path based on file name. All paths in the XBMLANGPATH variable
+    processed to see if the provided icon can be found.
+
+    :param str file_name:
+    :return: Icon path
+    :rtype: str/None
+    """
+    for directory in os.environ["XBMLANGPATH"].split(os.pathsep):
+        file_path = os.path.join(directory, file_name)
+        if os.path.exists(file_path):
+            return file_path.replace("\\", "/")
+
+
+def divider(parent):
+    """
+    :param QtWidgets.QWidget parent:
+    :rtype: QtWidgets.QFrame
+    """
+    line = QtWidgets.QFrame(parent)
+    line.setFrameShape(QtWidgets.QFrame.HLine)
+    line.setFrameShadow(QtWidgets.QFrame.Sunken)
+    return line
+
+
+def get_module_paths():
+    """
+    :return: Maya module paths
+    :rtype: list[str]
+    """
+    return os.environ["MAYA_MODULE_PATH"].split(os.pathsep)
+
+
+def get_module_file_paths():
+    """
+    :return: Maya module files
+    :rtype: list[str]
+    """
+    # variable
+    modules = []
+
+    # loop module paths
+    for path in get_module_paths():
+        # make sure path exists, by default maya adds paths to the variable
+        # that don't necessarily have to exist.
+        if not os.path.exists(path):
+            continue
+
+        # extend modules
+        modules.extend(
+            [
+                os.path.normpath(os.path.join(path, f))
+                for f in os.listdir(path) or []
+                if f.lower().endswith("mod") and not f.startswith("moduleManager")
+            ]
+        )
+
+    # sort modules by file name
+    modules.sort(key=lambda x: os.path.basename(x))
+
+    return modules
+
+
+def parse_module_line(line):
+    """
+    Parse the line of a module, the line needs to start with either a + or a -
+    if this is not the case it means it is additional information that belongs
+    to the module which is defined in the lines above this one. If that is the
+    case None is returned.
+
+    :param str line:
+    :return: Module data
+    :rtype: dict/None
+    """
+    # validate line
+    if len(line) < 1 or line[0] not in {"+", "-"}:
+        return
+
+    # variable
+    data = {}
+    partitions = line.split()
+
+    # copy partitions to loop and be able to remove from the original list
+    # without messing with the loop
+    for partition in reversed(partitions):
+        for argument in MODULE_ARGUMENTS:
+            if not partition.startswith(argument):
+                continue
+
+            data[argument] = partition[len(argument) + 1:]
+            partitions.remove(partition)
+
+    # validate length of partitions
+    if len(partitions) != 4:
+        return
+
+    # add additional data
+    for i, key in enumerate(["ENABLED", "NAME", "VERSION", "PATH"]):
+        data[key] = partitions[i]
+
+    return data
+
+
+def filter_module_file(file_path):
+    """
+    :param str file_path:
+    :return: Module data
+    :rtype: generator
+    """
+    # read module file
+    lines = read_module_file(file_path)
+
+    # filter content
+    for line in lines:
+        data = parse_module_line(line)
+        if not data:
+            continue
+
+        yield data
+
+
+def read_module_file(file_path):
+    """
+    :param str file_path:
+    :return: Module content
+    :rtype: list
+    """
+    # read file
+    with open(file_path, "r") as f:
+        lines = f.readlines()
+        lines = [x.strip() for x in lines]
+
+    return lines
+
+
+def update_module_file(file_path, state, data):
+    """
+    Update state of module, the module file gets read and the each line will
+    be checked if it matches up with the data provided. If it does, that is
+    the line that needs its state updated.
+
+    :param str file_path:
+    :param bool state:
+    :param dict data:
+    """
+    # prepare state
+    enabled = "+" if state else "-"
+
+    # prepare data for comparison
+    del data["ENABLED"]
+
+    # read existing file
+    content = []
+    lines = read_module_file(file_path)
+
+    for line in lines:
+        # parse line
+        line_data = parse_module_line(line)
+
+        # validate line
+        if line_data:
+            # remove enabled for comparison
+            del line_data["ENABLED"]
+
+            # validate line data with provided data
+            if data == line_data:
+                content.append(enabled + line[1:])
+                continue
+
+        # store original line
+        content.append(line)
+
+    # add new line to content
+    content = ["{}\n".format(c) for c in content]
+
+    # write to file
+    with open(file_path, "w") as f:
+        f.writelines(content)
+
+# ----------------------UI WIDGETS---------------------------------
 
 FONT = QtGui.QFont()
 FONT.setFamily("Consolas")
@@ -237,8 +449,8 @@ class MayaModuleFile(QtWidgets.QFrame):
         layout.addWidget(header)
 
         # create divider
-        divider = utils.divider(self)
-        layout.addWidget(divider)
+        _divider = divider(self)
+        layout.addWidget(_divider)
 
         # check permissions
         if not os.access(path, os.W_OK):
@@ -280,7 +492,7 @@ class MayaModuleFile(QtWidgets.QFrame):
         Populate the widget with module data widgets, one for each module data
         line found in the module file.
         """
-        for data in utils.filter_module_file(self.path):
+        for data in filter_module_file(self.path):
             mod = MayaModuleDetail(self, data)
             mod.enabled_changed.connect(self.update_module_file)
             self.layout().addWidget(mod)
@@ -292,7 +504,7 @@ class MayaModuleFile(QtWidgets.QFrame):
         :param bool state:
         :param dict data:
         """
-        utils.update_module_file(self.path, state, data)
+        update_module_file(self.path, state, data)
 
 
 # ----------------------------------------------------------------------------
@@ -337,13 +549,74 @@ class MayaModuleManager(QtWidgets.QWidget):
         Populate the widget with module file widgets, one for each module file
         found.
         """
-        for path in utils.get_module_file_paths():
+        for path in get_module_file_paths():
             mod = MayaModuleFile(self, path)
             self._layout.addWidget(mod)
 
 
-def show():
-    parent = utils.get_main_window()
+def show(*args, **kwargs):
+    parent = get_main_window()
     window = MayaModuleManager(parent)
     window.show()
     return window
+
+
+# -------------------------- PLUGIN setup ----------------------------------
+import sys
+import maya.api.OpenMaya as om
+import maya.cmds as cmds
+import maya.mel as mel
+
+
+# this plugin menu setup creates a single menu entry
+# to create a menu under Windows/my-tool
+
+# The below sample will create a new menu and menu-item: ToolsMenu/My cool tool
+# MENU_NAME is the name maya assigns to a menu, this is not always the same as the visible label
+# e.g. to parent to the default Maya menu 'Windows', use MENU_NAME="mainWindowMenu"
+MENU_NAME = "mainWindowMenu"  # no spaces in names, use CamelCase. Used to find and parent to a menu.
+# MENU_LABEL = "Tools"  # spaces are allowed in labels, only used when we create a new menu
+MENU_ENTRY_LABEL = "Module Manager"
+
+MENU_PARENT = "MayaWindow"  # do not change
+
+__menu_entry_name = "" # Store generated menu item, used when unregister
+
+
+def maya_useNewAPI():  # noqa
+    """dummy method to tell Maya this plugin uses the Maya Python API 2.0"""
+    pass
+
+
+def loadMenu():
+    """Setup the Maya menu, runs on plugin enable"""
+    global __menu_entry_name
+
+    # Maya builds its menus dynamically upon being accessed, so they don't exist if not yet accessed.
+    # We force a menu build to allow parenting any new menu under a default Maya menu
+    mel.eval("evalDeferred buildFileMenu")  # delete this if not parenting menus to a default Maya Menu
+
+    # if not cmds.menu(f"{MENU_PARENT}|{MENU_NAME}", exists=True):
+    #     cmds.menu(MENU_NAME, label=MENU_LABEL, parent=MENU_PARENT)
+    __menu_entry_name = cmds.menuItem(label=MENU_ENTRY_LABEL, command=show, parent=MENU_NAME)
+
+
+def unloadMenuItem():
+    """Remove the created Maya menu entry, runs on plugin disable"""
+    if cmds.menu(f"{MENU_PARENT}|{MENU_NAME}", exists=True):
+        menu_long_name = f"{MENU_PARENT}|{MENU_NAME}"
+        # Check if the menu item exists; if it does, delete it
+        if cmds.menuItem(__menu_entry_name, exists=True):
+            cmds.deleteUI(__menu_entry_name, menuItem=True)
+        # Check if the menu is now empty; if it is, delete the menu
+        if not cmds.menu(menu_long_name, query=True, itemArray=True):
+            cmds.deleteUI(menu_long_name, menu=True)
+
+
+# =============================== Plugin (un)load ===========================================
+def initializePlugin(plugin):
+    loadMenu()
+
+
+def uninitializePlugin(plugin):
+    unloadMenuItem()
